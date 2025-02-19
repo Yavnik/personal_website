0c880f62f5eefc1be527e6a621c55e22d3b840d9
Yavnik Sharma <17024368+Yavnik@users.noreply.github.com>
2025-02-19 22:03:04 +0530

    Add MDX components and styles for improved content rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,26 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import { SITE_URL } from './src/config/config';
 
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [
+    mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dark-plus",
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }),
+]
 });
