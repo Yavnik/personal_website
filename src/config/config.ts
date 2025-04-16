afc45d6ddc5012c235dd9e216e2e43264c90e29a
Yavnik Sharma <17024368+Yavnik@users.noreply.github.com>
2025-04-17 00:38:55 +0530

    Add Swetrix for analytics tracking and update site configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,10 @@
-export const SITE_TITLE = "YAVNIK SHARMA";
+export const SITE_TITLE = `${import.meta.env.PROD ? '' : 'DEV '}YAVNIK SHARMA`;
 export const SITE_DESC = "Yavnik Sharma's Tech Blog";
-export const SITE_URL = "https://yavniksharma.tech";
+export const SITE_URL =  `https://${import.meta.env.PROD ? '' : 'dev.'}yavniksharma.tech`;
 export const SITE_BANNER = "/banner.png";
 export const SITE_FAVICON = "/favicon.png";
 export const FOOTER_TEXT = "© 2025 Yavnik Sharma. All rights reserved.";
 export const HOME_QUOTE = "Welcome to my tech blog!";
+
+export const SWETRIX_PID = import.meta.env.SWETRIX_PID;
+export const SWETRIX_URL = import.meta.env.SWETRIX_URL;
