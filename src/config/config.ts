35c48cabfa43aef56ac88a72e4839f7b63c2834d
Yavnik Sharma <17024368+Yavnik@users.noreply.github.com>
2025-04-20 16:59:03 +0530

    chore: update project configuration and content
    - Added .env.development and .env.local to .gitignore
    - Removed unused images and updated robots.txt with new host URL
    - Updated site title and URL in config.ts
    - Added new blog post: "Every Developer Should Have Their Own Blog"
    - Deleted outdated blog posts and markdown style guide

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
-export const SITE_TITLE = `${import.meta.env.PROD ? '' : 'DEV '}YAVNIK SHARMA`;
+export const SITE_TITLE = `Yavnik's Blog`;
 export const SITE_DESC = "Yavnik Sharma's Tech Blog";
-export const SITE_URL =  `https://${import.meta.env.PROD ? '' : 'dev.'}yavniksharma.tech`;
+export const SITE_URL =  'https://yavnik.tech';
 export const SITE_BANNER = "/banner.png";
 export const SITE_FAVICON = "/favicon.png";
 export const FOOTER_TEXT = "© 2025 Yavnik Sharma. All rights reserved.";
