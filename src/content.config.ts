dc0667363d9fcd04da5d89503e066bd7f35e5daa
Yavnik Sharma <17024368+Yavnik@users.noreply.github.com>
2025-08-09 13:25:18 +0530

    Modified generated Id for blog logic and fixed date component

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,19 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-    loader: glob({pattern: '**/*.{md,mdx}', base: './src/content/blog'}),
-    schema: z.object({
-            title: z.string(),
-            description: z.string(),
-            pubDate: z.coerce.date(),
-            heroImage: z.string(),
-            tags: z.array(z.string()).optional(),
-        }),
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/blog",
+    generateId: ({ entry }) => entry.replace(/\.(md|mdx)$/, ""),
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    heroImage: z.string(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
-export default { blog };
+export const collections = { blog };
